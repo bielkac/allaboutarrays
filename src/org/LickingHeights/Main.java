78d804b2086af9d8b3203cecd8a9fbb4ed338825
Bielka Cordero <bielka.corderohuerta.2022@student.lhschools.org>
2019-12-12 09:54:46 -0500

    completed pattern1

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,101 @@
+package org.LickingHeights;
+
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int size = 10;
+        int lowestnumber = 0;
+        int highestNumber = 10 ;
+
+
+        int[] array = createNumberArray(size, lowestnumber, highestNumber);
+
+        Pattern1();
+    }
+
+    public static int[] createNumberArray(int size, int lowestNumber, int highestNumber) {
+        int[] array = new int[size];
+
+        //fill with random values
+        for (int i = 0; i < size; i++) {
+            array[i] = createRandomNumber(lowestNumber, highestNumber);
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static int createRandomNumber(int lowestNumber,int highestNumber) {
+
+        int number = (int) (Math.random() * (highestNumber - lowestNumber + 1)) + lowestNumber;
+        return number;
+    }
+
+    public static void Pattern1() {
+        for (int rows = 0; rows < 6; rows++) {
+
+            for (int columns = 0; columns < 6; columns++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+    }
+    public static void Pattern2 () {
+        for (int rows = 1; rows <= 6; rows++) {
+
+
+            for (int columns = 0; columns < 6; columns++) {
+
+
+                System.out.print(rows);
+            }
+            System.out.println();
+        }
+    }
+    public static void Pattern3() {
+        for (int rows = 1; rows <=6; rows ++) {
+
+            for (int columns = 1; columns <=6; columns ++) {
+
+                System.out.print(columns);
+
+            }
+            System.out.println();
+        }
+    }
+    public static void Pattern4 () {
+        for (int rows = 1; rows <= 6; rows++) {
+
+            for (int columns = 1; columns<=rows; columns++) {
+
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+    public static void Pattern5 () {
+        for (int rows = 1; rows <=6; rows ++) {
+
+            for (int columns = 1; columns <=rows; columns++) {
+
+                
+                System.out.print(columns);
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
